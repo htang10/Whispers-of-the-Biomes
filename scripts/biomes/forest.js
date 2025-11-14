d3f9726c50696693d53d85858395e55b2b44d2e4
Huy Tang <htang.prof@outlook.com>
2025-11-13 16:12:16 -0800

    Reformatted small portions of the code to maintain consistent format

--- scripts/biomes/forest.js
+++ scripts/biomes/forest.js
@@ -2,5 +2,5 @@ import { createBlurCircle } from "./biomeInterface.js";
 
 const TREE = "This is a majestic oak tree, standing tall and proud in the heart of the forest.";
 
-createBlurCircle(75, 65,TREE);
+createBlurCircle(75, 65, TREE);
 createBlurCircle(90, 30, TREE);
