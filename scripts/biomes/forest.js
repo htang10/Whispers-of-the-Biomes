93b4c0dc5d6e516f599c86fd99edcc871f8c247e
Huy Tang <htang.prof@outlook.com>
2025-11-17 17:54:22 -0800

    Updated the function name for setting up sound for each biome

--- scripts/biomes/forest.js
+++ scripts/biomes/forest.js
@@ -1,9 +1,9 @@
-import { setUpSound } from "../utils/sound.js";
+import { setUpSoundBiome } from "../utils/sound.js";
 import { createBlurCircle } from "./biomeInterface.js";
 
 const AUDIO_SOURCE = "../../assets/forest/forest-bg-music.m4a";
 const TREE = "This is a majestic oak tree, standing tall and proud in the heart of the forest.";
 
-setUpSound(AUDIO_SOURCE);
+setUpSoundBiome(AUDIO_SOURCE);
 createBlurCircle(75, 65, TREE);
 createBlurCircle(90, 30, TREE);
